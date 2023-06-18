d885235a920d256fd0e9c8cc78e8a63dba3f1a03
Stroh Snow <github@stroh.ru>
2023-06-18 20:37:37 +0300

    Fixed hash probing table

--- table/hash_probing/table.c
+++ table/hash_probing/table.c
@@ -5,9 +5,9 @@
 int table_find(const Table *table, const int key)
 {
     int i = hash_int(key) % table->msize, n = 0;
-    while (n < table->msize && table->ks[i].busy)
+    while (n < table->msize && table->ks[i].busy != 0)
     {
-        if (table->ks[i].key == key)
+        if (table->ks[i].busy == 1 && table->ks[i].key == key)
             return i;
 
         i = (i + 1) % table->msize;
@@ -23,7 +23,7 @@ int table_insert(Table *table, const int key, const char *info)
         return DUPLICATE_KEY;
     
     int i = hash_int(key) % table->msize, n = 0;
-    while (n < table->msize && table->ks[i].busy)
+    while (n < table->msize && table->ks[i].busy == 1)
     {
         i = (i + 1) % table->msize;
         ++n;
@@ -46,7 +46,7 @@ int table_delete(Table *table, const int key)
     if (i < 0)
         return KEY_NOT_FOUND;
 
-    table->ks[i].busy = 0;
+    table->ks[i].busy = -1;
     free(table->ks[i].data->info);
     free(table->ks[i].data);
 
